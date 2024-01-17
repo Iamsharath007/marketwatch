0a282a3007dd022a50846d5e047e2e6f4d865162
idhanush <idhanushm@gmail.com>
2024-01-17 23:41:20 +0530

    if else refac

--- app.py
+++ app.py
@@ -31,27 +31,26 @@ def get_data(_id, start, end):
     result = instrument_keys[instrument_keys['name'] == f"{_id}"]["instrument_key"].iloc[0]
     response = upstock_app.get_historical_candle_data1(result, 'day', end, start, 'v2')
     # Check the response status
-    if response.status == "success":
-        # Do something with the response data (e.g., print it)
-        datas = response.data.candles
-        data_dict = {"Date": [], "Open": [], "Close": [], "Percentage": []}
-        for data in datas:
-            date = data[0].split("T")[0]
-            data_dict["Date"].append(date)
-            data_dict["Open"].append(data[1])
-            data_dict["Close"].append(data[4])
-            data_dict["Percentage"].append(f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%")
-        data_df = pd.DataFrame(data_dict)
-        data_df.to_excel(f"{_id}.xlsx", index=False)
-
-    else:
-        # Print an error message if the request was not successful
+    if response.status != "success":
         print(f"Error: {response.status_code} - {response.text}")
+        return
+    datas = response.data.candles
+    data_dict = {"Date": [], "Open": [], "Close": [], "Percentage": []}
+    for data in datas:
+        date = data[0].split("T")[0]
+        data_dict["Date"].append(date)
+        data_dict["Open"].append(data[1])
+        data_dict["Close"].append(data[4])
+        data_dict["Percentage"].append(f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%")
+    data_df = pd.DataFrame(data_dict)
+    data_df.to_excel(f"{_id}.xlsx", index=False)
+
 
 
 
 @app.get("/download")
 def download_file(filename: str):
+
     return FileResponse(filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                         filename=filename)
 
