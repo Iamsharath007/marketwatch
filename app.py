58e01fab5fa5c0e264103eacc739387cea93a0d0
idhanush <idhanushm@gmail.com>
2024-01-17 23:52:30 +0530

    home page update

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+import io
+from fastapi import FastAPI, Response, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import requests
 from fastapi.responses import FileResponse
-# from nsetools import Nse
 import upstox_client
 import pandas as pd
-
+import uvicorn
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 instrument_keys = pd.read_csv('instrument_keys.csv')
 instrument_keys = instrument_keys[["instrument_key", "name"]]
+templates = Jinja2Templates(directory="templates")
 
 def download_file(filename: str):
     return FileResponse(filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -24,6 +26,13 @@ app.add_middleware(
     allow_methods=["*"],  # Set this to the specific HTTP methods you want to allow
     allow_headers=["*"],  # Set this to the specific HTTP headers you want to allow
 )
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    try:
+        return templates.TemplateResponse("index.html", {"request": request})
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="index.html not found")
 
 
 @app.get('/data')
@@ -44,14 +53,20 @@ def get_data(_id, start, end):
         data_dict["Percentage"].append(f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%")
     data_df = pd.DataFrame(data_dict)
     data_df.to_excel(f"{_id}.xlsx", index=False)
-
-
+    excel_buffer = io.BytesIO()
+    data_df.to_excel(excel_buffer, index=False)
+    excel_buffer.seek(0)
+    return Response(
+        content=excel_buffer.read(),
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        headers={"Content-Disposition": f"attachment; filename={_id}.xlsx"}
+    )
 
 
 @app.get("/download")
 def download_file(filename: str):
-
     return FileResponse(filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                         filename=filename)
 
 
+uvicorn.run(app)
