5a499f3cd870026456dd44dcd411c545eaa2e2c3
Iamsharath007 <sharathpournami@gmail.com>
2024-01-17 23:19:29 +0530

    download api changed

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 from fastapi.responses import FileResponse
+# from nsetools import Nse
 import upstox_client
 import pandas as pd
-import uvicorn
-from starlette.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
-
-upstock_app = upstox_client.HistoryApi()
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"]
-)
 
 instrument_keys = pd.read_csv('instrument_keys.csv')
 instrument_keys = instrument_keys[["instrument_key", "name"]]
-
 
 def download_file(filename: str):
     return FileResponse(filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                         filename=filename)
 
 
-@app.get('/')
-def home_page(request: Request):
-    try:
-        return templates.TemplateResponse("index.html", {"request": request})
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="index.html not found")
+upstock_app = upstox_client.HistoryApi()
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to the specific domain(s) you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],  # Set this to the specific HTTP methods you want to allow
+    allow_headers=["*"],  # Set this to the specific HTTP headers you want to allow
+)
 
 
 @app.get('/data')
 def get_data(_id, start, end):
     result = instrument_keys[instrument_keys['name'] == f"{_id}"]["instrument_key"].iloc[0]
     response = upstock_app.get_historical_candle_data1(result, 'day', end, start, 'v2')
+    # Check the response status
     if response.status == "success":
+        # Do something with the response data (e.g., print it)
         datas = response.data.candles
         data_dict = {"Date": [], "Open": [], "Close": [], "Percentage": []}
         for data in datas:
@@ -47,6 +43,7 @@ def get_data(_id, start, end):
             data_dict["Percentage"].append(f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%")
         data_df = pd.DataFrame(data_dict)
         data_df.to_excel(f"{_id}.xlsx", index=False)
+
     else:
         # Print an error message if the request was not successful
         print(f"Error: {response.status_code} - {response.text}")
@@ -56,6 +53,3 @@ def get_data(_id, start, end):
 def download_file(filename: str):
     return FileResponse(filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                         filename=filename)
-
-
-uvicorn.run(app)
