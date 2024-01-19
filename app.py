eb448eac36e481a576ce858ce0b3a42b27d4b84b
idhanush <idhanushm@gmail.com>
2024-01-19 15:44:48 +0530

    optimsed

--- app.py
+++ app.py
@@ -7,11 +7,27 @@ import pandas as pd
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
-instrument_keys = pd.read_csv('instrument_keys.csv')
-instrument_keys = instrument_keys[["instrument_key", "name"]]
+# Load instrument keys and extract symbol names
+instrument_keys = pd.read_csv('instrument_keys.csv')[["instrument_key", "name"]]
 symbol_names = instrument_keys['name']
 
+# Create Jinja2 templates instance
 templates = Jinja2Templates(directory="templates")
+
+# Create Upstox API instance
+upstox_app = upstox_client.HistoryApi()
+
+# FastAPI application
+app = FastAPI()
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 def search_suggestions(target):
@@ -19,49 +35,47 @@ def search_suggestions(target):
         target = target.lower()
         matches = {word for word in symbol_names if target in str(word).lower()}
         return matches
-    except:
+    except Exception as e:
+        print(f"Error in search_suggestions: {e}")
         return {}
 
 
 def extractor(_id, start, end, instrument_keys, data_dict):
-    result = instrument_keys[instrument_keys['name'] == f"{_id}"]["instrument_key"].iloc[0]
-    response = upstock_app.get_historical_candle_data1(result, 'day', end, start, 'v2')
-    # Check the response status
-    if response.status != "success":
-        print(f"Error: {response.status_code} - {response.text}")
-        return
-    datas = response.data.candles
+    try:
+        result = instrument_keys.loc[instrument_keys['name'] == f"{_id}", "instrument_key"].iloc[0]
+        response = upstox_app.get_historical_candle_data1(result, 'day', end, start, 'v2')
 
-    print(data_dict)
-    for data in datas:
-        date = data[0].split("T")[0]
+        if response.status != "success":
+            raise HTTPException(status_code=500, detail=f"Error in extractor: {response.status_code} - {response.text}")
 
-        # Check if the key exists, create it if not
-        if (date, 'open') not in data_dict:
-            data_dict[(date, 'open')] = {}
-        if (date, 'close') not in data_dict:
-            data_dict[(date, 'close')] = {}
-        if (date, 'percen') not in data_dict:
-            data_dict[(date, 'percen')] = {}
+        datas = response.data.candles
 
-        # Assign values to the keys
-        data_dict[(date, 'open')][_id] = data[1]
-        data_dict[(date, 'close')][_id] = data[4]
-        data_dict[(date, 'percen')][_id] = f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%"
+        for data in datas:
+            date = data[0].split("T")[0]
 
-    data_df = pd.DataFrame.from_dict(data_dict)
-    data_df.to_excel('output.xlsx')
+            # Check if the key exists, create it if not
+            for key in ['open', 'close', 'percen']:
+                if (date, key) not in data_dict:
+                    data_dict[(date, key)] = {}
+
+            # Assign values to the keys
+            data_dict[(date, 'open')][_id] = data[1]
+            data_dict[(date, 'close')][_id] = data[4]
+            data_dict[(date, 'percen')][_id] = f"{round(((data[4] - data[1]) / data[1]) * 100, 3)}%"
+
+    except Exception as e:
+        print(f"Error in extractor for {_id}: {e}")
 
 
-upstock_app = upstox_client.HistoryApi()
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Set this to the specific domain(s) you want to allow
-    allow_credentials=True,
-    allow_methods=["*"],  # Set this to the specific HTTP methods you want to allow
-    allow_headers=["*"],  # Set this to the specific HTTP headers you want to allow
-)
+def get_excel_response(data_df):
+    excel_buffer = io.BytesIO()
+    data_df.to_excel(excel_buffer)
+    excel_buffer.seek(0)
+    return Response(
+        content=excel_buffer.read(),
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        headers={"Content-Disposition": "attachment; filename=result.xlsx"}
+    )
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -76,26 +90,18 @@ async def read_root(request: Request):
 async def get_data(companies: str, start: str, end: str):
     data_dict = {}
     companies = companies.split(',')
-    print(companies)
+
     for company in companies:
         extractor(company, start, end, instrument_keys, data_dict)
+
     data_df = pd.DataFrame.from_dict(data_dict)
-    excel_buffer = io.BytesIO()
-    data_df.to_excel(excel_buffer)
-    excel_buffer.seek(0)
-    return Response(
-        content=excel_buffer.read(),
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        headers={"Content-Disposition": f"attachment; filename=result.xlsx"}
-    )
+    return get_excel_response(data_df)
 
 
 @app.get('/suggest')
 async def suggest(query: str):
     if not query:
         return {'suggestions': {}}
+
     results = search_suggestions(query)
     return {'suggestions': results}
-
-
-
